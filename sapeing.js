1c3395e7a06ecfc04547f074bc97f7382296b77c
Apolophone <36250650+priladeva@users.noreply.github.com>
2018-02-19 22:45:14 +0000

    Update sapeing.js

--- sapeing.js
+++ sapeing.js
@@ -18,7 +18,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://www.link-ismoney.ga/p/convert.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*7)+ 1);
+var xxx=null;
+if(x=="1"){xxx="if-chocolate-is-fattening.html"}
+if(x=="2"){xxx="blog-post.html"}
+if(x=="3"){xxx="7c-squad.html"}
+if(x=="7"){xxx="working.html"}a_to_ve[i].href="http://www.link-ismoney.ga/1999/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
